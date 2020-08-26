36438805e8dfe9b1c319761b56bd2e22b3543a9c
Brian Charles <bcharles41078@gmail.com>
2020-08-26 14:46:00 -0500

    try to revert

--- src/steps/steps-router.js
+++ src/steps/steps-router.js
@@ -7,18 +7,18 @@ const jsonBodyParser = express.json()
 const StepsRouter = express.Router()
 
 StepsRouter
-    // .route('/')
-    // .get(requireAuth, async (req, res, next) => {
-    //     try {
-    //         const steps = await StepsService.getAllSteps(
-    //             req.app.get('db'),
-    //             req.user.id
-    //         );
-    //         res.status(200).json(
-    //             steps.map(step => ProjectsService.serializeSteps(step))
-    //         );
+    .route('/')
+    .get(requireAuth, async (req, res, next) => {
+        try {
+            const steps = await StepsService.getAllSteps(
+                req.app.get('db'),
+                req.user.id
+            );
+            res.status(200).json(
+                steps.map(step => ProjectsService.serializeSteps(step))
+            );
 
-    //     } catch (error) {
-    //         next(error);
-    //     }
-    // })
+        } catch (error) {
+            next(error);
+        }
+    });
